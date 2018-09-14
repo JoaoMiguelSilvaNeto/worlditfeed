ac48898b2a62f320d29dce17938713945fbe75f4
João Miguel da Silva Neto <joao.miguel.silva.neto@gmail.com>
2018-09-14 17:01:36 +0100

    CWU

---
+++ js/settings.js
@@ -0,0 +1,15 @@
+// Javascript file for settings.html
+
+$(document).ready(function(){
+	
+	// Save Settings
+	
+	$("#category").getCtrl().onChange(function (value) {
+		console.log(value);
+    });
+	
+	//getValue function
+	//console.log($("#sampleCheckbox").getCtrl().getValue());
+ 
+ 
+});
