d42f781b1b906445704b650f886064a8ead09529
João Miguel da Silva Neto <joao.miguel.silva.neto@gmail.com>
2018-09-17 12:45:12 +0100

    CWU

--- js/settings.js
+++ js/settings.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	
 	var categories = {"title": 'Category', "options": [{ "value": 'Noticias', "label": 'Notícias'},{ "value": 'Tweets', "label": 'Tweets'},{ "value": 'Outras', "label": 'Outras'}],  "defaultValue": 'Noticias', "InfoText": 'Category of the feed to show.'};
 	
-	$("#category").attr("wix-options","'title': 'Category', 'defaultValue': 'Noticias', 'options': [{ value: 'Noticias', label: 'Notícias'},{ value: 'Tweets', label: 'Tweets'},{ value: 'Outras', label: 'Outras'}], 'InfoText': 'Category of the feed to show.'");
+	$("#category").attr("wix-options","{'title': 'Category', 'defaultValue': 'Noticias', 'options': [{ 'value': 'Noticias', 'label': 'Notícias'},{ 'value': 'Tweets', 'label': 'Tweets'},{ 'value': 'Outras', 'label': 'Outras'}], 'InfoText': 'Category of the feed to show.'}");
 	//$("#category").getCtrl().wix-options(categories);
 	//getValue function
 	//console.log($("#sampleCheckbox").getCtrl().getValue());
