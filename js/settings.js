af48b0ec1d440134f088e3c6dd6ac1eaa03ff121
João Miguel da Silva Neto <joao.miguel.silva.neto@gmail.com>
2018-09-17 12:00:47 +0100

    CWU

--- js/settings.js
+++ js/settings.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	var categories = {title: 'Category', options: [{ value: 'Noticias', label: 'Notícias'},{ value: 'Tweets', label: 'Tweets'},{ value: 'Outras', label: 'Outras'}],  defaultValue: 'Noticias', InfoText: 'Category of the feed to show.'};
 	
 	//$("#category").attr("wix-options","{title: 'Category', defaultValue: 'Noticias', options: [{ value: 'Noticias', label: 'Notícias'},{ value: 'Tweets', label: 'Tweets'}{ value: 'Outras', label: 'Outras'}]}");
-	$("#category").wix-options(categories);
+	$("#category").getCtrl().wix-options(categories);
 	//getValue function
 	//console.log($("#sampleCheckbox").getCtrl().getValue());
  
