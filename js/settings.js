60b6ca1cab470954f07ee62277fd755a6e789d83
João Miguel da Silva Neto <joao.miguel.silva.neto@gmail.com>
2018-09-17 15:26:32 +0100

    CWU

--- js/settings.js
+++ js/settings.js
@@ -1,22 +1,6 @@
 // Javascript file for settings.html
-//$("#category").attr("wix-options","{title: 'Category', defaultValue: 'Noticias', options: [{ 'value': 'Noticias', 'label': 'Notícias'},{ 'value': 'Tweets', 'label': 'Tweets'},{ 'value': 'Outras', 'label': 'Outras'}], InfoText: 'Category of the feed to show.'}");
-	
-window.onload = function() {
-		
-		document.getElementById('category').setAttribute("wix-options", "{title: 'Category', " +
-				"defaultValue: 'Noticias', " +
-				"options: [" +
-				"	{ value: 'Noticias', label: 'Notícias'}," +
-				"	{ value: 'Tweets', label: 'Tweets'}," +
-				"	{ value: 'Outras', label: 'Outras'}" +
-				"], " +
-				"InfoText: 'Category of the feed to show.'" +
-				"}");
-		}
 
 $(document).ready(function(){
-	
-	
 	
 	// Save Settings
 	
@@ -27,7 +11,16 @@ $(document).ready(function(){
 	
 	// Retrieve settings
 	
-	var categories = {"title": 'Category', "options": [{ "value": 'Noticias', "label": 'Notícias'},{ "value": 'Tweets', "label": 'Tweets'},{ "value": 'Outras', "label": 'Outras'}],  "defaultValue": 'Noticias', "InfoText": 'Category of the feed to show.'};
+	function getCategories()	{
+		
+		//Call WebService that returns Categories
+		
+		var categories = [{ value: 'Noticias', label: 'Notícias'},{ value: 'Tweets', label: 'Tweets'},{value: 'Outras', label: 'Outras'}];
+		
+		return categories;
+		
+	}
+	
 	
 	
 	
