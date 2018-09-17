6d3a19140a2ea5d52b183db5c7a362a700e760a5
João Miguel da Silva Neto <joao.miguel.silva.neto@gmail.com>
2018-09-17 15:36:51 +0100

    CWU

--- js/settings.js
+++ js/settings.js
@@ -40,7 +40,7 @@ function getCategories()	{
 	
 	//Call WebService that returns Categories
 	
-	var categories = [{ value: 'Noticias', label: 'Notícias'},{ value: 'Tweets', label: 'Tweets'},{value: 'Outras', label: 'Outras'}];
+	var categories = "[{ value: 'Noticias', label: 'Notícias'},{ value: 'Tweets', label: 'Tweets'},{value: 'Outras', label: 'Outras'}]";
 	
 	return categories;
 	
