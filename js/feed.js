60dcf68f607f1e1f658949d97c6efd0acba5b2de
João Miguel da Silva Neto <joao.miguel.silva.neto@gmail.com>
2018-09-20 15:10:11 +0100

    CWU

--- js/feed.js
+++ js/feed.js
@@ -79,7 +79,7 @@ function getToken()	{
 */
 function setFeed(updates)	{
 
-	var html = '<tr><td colspan="2"><h2 id="title" class="sample-content-title">Notícias</h2></td></tr>';
+	var html = '';
 	
 	updates.forEach(function (entry)	{
 		
